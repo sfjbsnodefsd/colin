fd64d6f2f1c542671bf2e5febb2d0c32122f7523
sfjbssept <sfjbssept@gmail.com>
2022-09-28 15:26:18 +0000

    statements working

--- node-js/JWT-Auth-Project/api/users/user.router.js
+++ node-js/JWT-Auth-Project/api/users/user.router.js
@@ -1,6 +1,10 @@
-const {createUser} = require("./user.controller");
+const {createUser, deleteUser, getUserById, getUsers, updateUser} = require("./user.controller");
 const router = require("express").Router();
 
 router.post("/", createUser);
+router.get("/", getUsers);
+router.get("/:id", getUserById);
+router.patch("/", updateUser);
+router.delete("/", deleteUser);
 
 module.exports = router;
--- node-js/JWT-Auth-Project/api/users/user.service.js
+++ node-js/JWT-Auth-Project/api/users/user.service.js
@@ -24,7 +24,7 @@ module.exports = {
 
     getUsers: callBack => {
         pool.query(
-            'select id,firstname,lastname,gender,email,number from registration',
+            'select id,firstName,lastName,gender,email,number from registration',
             [],
             (error, results, fields) => {
                 if (error) {
@@ -38,7 +38,7 @@ module.exports = {
 
     getUserById: (id,callBack) => {
         pool.query(
-            'select id,firstname,lastname,gender,email,number from registration where id = ?',
+            'select id,firstName,lastName,gender,email,number from registration where id = ?',
             [id],
             (error, results, fields) => {
                 if (error) {
@@ -48,5 +48,40 @@ module.exports = {
             }
 
         )
+    },
+
+    updateUser: (data,callBack) => {
+        pool.query(
+            'update registration set firstName =? , lastName =? , gender=? , email =? , password =? , number =? where id = ?',
+            [
+                
+                data.first_name,
+                data.last_name,
+                data.gender,
+                data.email,
+                data.password,
+                data.number,
+                data.id
+
+            ], (error, results, fields) => {
+                if(error) {
+                    callBack(error);
+                }
+                return callBack(null, results)
+            }
+        );
+    },
+
+    deleteUser: (data, callBack) => {
+        pool.query(
+            'delete from registration where id = ?',
+            [data.id],
+            (error, results, fields) => {
+                if(error) {
+                    return callBack(error)
+                } return callBack(null, results[0])
+            }
+        )
     }
+
 };
