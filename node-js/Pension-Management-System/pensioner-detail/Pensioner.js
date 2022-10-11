27a6bc9d1aa806f3794348842dd63081f9b9cb6f
colin osullivan <colinosullivan@tcs.com>
2022-10-11 14:27:53 +0000

    pension project, jwt added, most of pensioner-detail added

--- node-js/Pension-Management-System/pensioner-detail/Pensioner.js
+++ node-js/Pension-Management-System/pensioner-detail/Pensioner.js
@@ -5,6 +5,7 @@ const ProductSchema = new Schema({
     name: String,
     date_of_birth: String,
     pan: Number,
+    salary_earned: Number,
     allowences: Number,
     self_family_pension: String,
     bank_detail: {
